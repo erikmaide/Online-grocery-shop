002f63d48829f060ca3f977f7898c783845aee69
Erik Maide <maide.erik@gmail.com>
2021-12-18 22:06:29 +0200

    added additional css for sidebar

--- src/Cart/Cart.styles.tsx
+++ src/Cart/Cart.styles.tsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.aside`
   font-family: Arial, Helvetica, sans-serif;
-  width: 400px;
+  width: 30vw;
   padding: 20px;
+
+  @media only screen and (max-width: 600px) {
+    width: 70vw;
+  }
 `;

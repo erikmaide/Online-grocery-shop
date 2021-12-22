d98228d8110490017c9ef775b54171f7e121ab37
Erik Maide <maide.erik@gmail.com>
2021-12-23 01:24:22 +0200

    refactored itempage code

--- src/ItemPage/ItemPage.tsx
+++ src/ItemPage/ItemPage.tsx
@@ -5,30 +5,29 @@ import { Wrapper } from './ItemPage.styles';
 import { Link } from 'react-router-dom';
 
 type Props = {
-    addToCart: (clickedItem: CartItemType) => void;
-    data: any;
-  };
+  addToCart: (clickedItem: CartItemType) => void;
+  data: any;
+};
 
-  const ItemPage: React.FC<Props> = ({ data, addToCart }) => {
-    const {itemId} = useParams();
-    const item: CartItemType = data?.find((item: { id: string | undefined; }) =>(item.id === itemId));
-    return (
+const ItemPage: React.FC<Props> = ({ data, addToCart }) => {
+  const { itemId } = useParams();
+  const item: CartItemType = data ?.find((item: { id: string | undefined; }) => (item.id === itemId));
+  return (
     <Wrapper>
-    <img src={item.image || "https://tinyurl.com/nosushipic"} alt={item.name} style={{height: "200px"}} />
+      <img src={item.image || "https://tinyurl.com/nosushipic"} alt={item.name} style={{ height: "200px" }} />
       <h2>{item.name}</h2>
       <h2>Price: {item.price || "-"}€</h2>
       <h2>Rating: {item.rating || "-"}</h2>
       <h2>Ingredients: {item.description || "-"}</h2>
-    <Button className="buttonDetailed">
+      <Button className="buttonDetailed">
         <Link to="/" className="buttonDetailed">Back</Link>
-    </Button>
+      </Button>
       {item.price > 0 &&
         <Button onClick={() => addToCart(item)} className="button" >
           Add to Cart
         </Button>
       }
-         </Wrapper>
-        
+    </Wrapper>
   );
 }
 export default ItemPage;
